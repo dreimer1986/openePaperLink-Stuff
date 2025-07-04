513d12433f854965c855b40417a4a5e7b71f5300
Daniel Reimer <daniel.reimer@reactos.org>
2025-07-04 16:10:52 +0200

    Update randompic.py
    Put into account if the input is portrait or landscape and try to crop accordingly to fill the ePaper, but show as much as possible anyways.

--- Tags/atc1441's M3 12.2/randompic.py
+++ Tags/atc1441's M3 12.2/randompic.py
@@ -7,14 +7,22 @@ except:
     from PIL import Image
 
 mywidth = 960
+myheight = 768
 
 os.chdir('./media/')
 random_file=random.choice(os.listdir("."))
 
 img = Image.open(random_file)
-wpercent = (mywidth/float(img.size[0]))
-hsize = int((float(img.size[1])*float(wpercent)))
-img = img.resize((mywidth,hsize),1)
+
+if img.size[0] > img.size[1]:
+    wpercent = (myheight/float(img.size[1]))
+    vsize = int((float(img.size[0])*float(wpercent)))
+    img = img.resize((vsize,myheight),1)
+else:
+    wpercent = (mywidth/float(img.size[0]))
+    hsize = int((float(img.size[1])*float(wpercent)))
+    img = img.resize((mywidth,hsize),1)
+
 img = img.convert('RGB')
 img.save('./frame_temp/tempframe.jpg', quality=95)
 
